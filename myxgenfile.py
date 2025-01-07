3fedd47c01573457034482d35d43886a82905cac
myxln000 <myx28123899632021@outlook.com>
2025-01-07 12:29:45 +0800

    finish the base copy functions and finished thr keil_config.ini changes,there need the text changer in genbinary.bat file

---
+++ myxgenfile.py
@@ -0,0 +1,86 @@
+import os
+import shutil
+import sys
+import re
+
+def createProj(pre_path):
+    if not os.path.exists("./project"):
+        os.makedirs("./project/incs")
+    else:
+        print("Poject is already exist!")
+        sys.exit(1)
+    basefilelist = [
+        "./inc",
+        "./src",
+        "./cfg",
+        "./utils"
+    ]
+    usedforincs = [
+        "drivers",
+        "lora",
+        "platform"
+    ]
+    for file in basefilelist:
+        shutil.copytree(file,"./project/"+file)
+    for file in usedforincs:
+        shutil.copytree(pre_path + file, "./project/incs/"+file)
+    shutil.copy('./keil.bat','./project/keil.bat')
+
+
+def genKeilconf(pre_path):
+    keil_config_file_path = "./project/utils/keil_config.ini"
+    try:
+        pre_path = pre_path[:-2]
+        with open(keil_config_file_path, encoding='utf-8') as conf:
+            content = conf.read()
+
+        # split content to use
+        datas = content.split('\n')
+        src_paths = datas[1][5:-2].split("', '")
+        inc_paths = datas[3][14:-2].split("; ")
+
+        # replace the paths
+        changeRule_src = [
+            (pre_path + r"projects/.*/examples/.*/src/(.*?)", "./src/"),
+            (pre_path + r"platform/(.*?)", "./incs/platform/"),
+            (pre_path + r"drivers/peripheral/(.*?)", "./incs/drivers/peripheral/")
+        ]
+        changeRule_inc = [
+            (pre_path + r"projects/.*/examples/.*/inc", "./inc"),
+            (pre_path + r"platform/(.*?)", "./incs/platform/"),
+            (pre_path + r"drivers/peripheral/(.*?)", "./incs/drivers/peripheral/")
+        ]
+
+        for i, path in enumerate(src_paths):
+            for pattern, replacement in changeRule_src:
+                st = re.sub(pattern, replacement, path)
+                if not src_paths[i] == st:
+                    src_paths[i] = st
+                    break
+        for i, path in enumerate(inc_paths):
+            for pattern, replacement in changeRule_inc:
+                st = re.sub(pattern, replacement, path)
+                if not inc_paths[i] == st:
+                    inc_paths[i] = st
+                    break
+
+        # merge the changes and write to file
+        st = ""
+        for path in src_paths:
+            st = st + "'" + path + "', "
+        datas[1] = "src=" + st
+        st = "'"
+        for path in inc_paths:
+            st = st + path + '; '
+        datas[3] = "include_path=" + st + "'"
+        st = ''
+        for i in datas:
+            st = st + i + '\n'
+
+        # 将替换后的内容写回文件
+        with open(keil_config_file_path, 'w') as file:
+            file.write(st)
+
+    except:
+        print("can not open generate file in ./project/utils/keil_config.in")
+        sys.exit(1)
---
+++ readme.md
@@ -0,0 +1,37 @@
+# ASR6601 keil GEN helper
+
+**brief:**
+
+用于给ASR6601的sdk生成可移动keil工程例程
+
+测试过的版本： ASR6601_LoRaWAN 1.0.3 SDK
+
+Generate movable keil project for ASR6601 in sdk
+
+Used for ASR6601 Keil project generate to a movable file
+
+suitable version: ASR6601_LoRaWAN 1.0.3 SDK
+
+## Usage 
+
+1、解压SDK文件，打开"projects"文件夹找到你要打开的文件
+
+2、直接复制Genhelper.exe程序到目标文件夹下，放在keil.bat文件一起，双击运行.
+
+3、此时会生成一个Project文件夹下面是生成好的可移动文件
+
+1.unzip the SDK zip file, open the direct "projects" and find the project you want
+
+2.Copy the Genhelper.exe to the project file which you want to use, and put it with the "keil.bat" file,click it.
+
+## the file structure
+
+Briefly, you only should copy these file to your Project
+
+```
+./incs/
+./
+```
+
+
+
